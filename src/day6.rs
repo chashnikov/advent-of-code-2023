f03cac82df8c1c60b9a13b93eae7bde7fd818007
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2023-12-09 12:07:11 +0100

    [day6] solve first part

---
+++ src/day6.rs
@@ -0,0 +1,43 @@
+use std::fs;
+use std::path::Path;
+use crate::string_to_numbers;
+
+pub fn day6() {
+    let content = fs::read_to_string(Path::new("6-full.txt")).expect("input must exist");
+    let mut times : Vec<u64> = Vec::new();
+    let mut distances : Vec<u64> = Vec::new();
+    content.lines().for_each(|line| {
+        if line.starts_with("Time:") {
+            times = string_to_numbers(&line[5..]);
+        }
+        else if line.starts_with("Distance:") {
+            distances = string_to_numbers(&line[9..])
+        }
+    });
+    let answer : u64 = times.iter().zip(distances.iter()).map(|(time, distance)| {
+        let d2 = time * time - distance * 4;
+        if (d2 <= 0) {
+            0
+        }
+        else {
+            let s2 : f64 = (d2 as f64).sqrt();
+            let s2_int = s2.round() as u64;
+            if s2_int * s2_int == d2 && (time + s2_int) % 2 == 0 {
+                s2_int - 1
+            }
+            else {
+                let f_time = *time as f64;
+                (((f_time + s2)/ 2_f64).floor() as u64) - (((f_time - s2)/ 2_f64).ceil() as u64) + 1
+            }
+        }
+    }).product();
+    /*
+      time, distance:
+      t: [0, time] -> t*(time-t) > distance
+      t^2 - t*time + distance < 0
+      (t-time/2)^2 + distance - time^2/4 < 0
+      abs(t-time/2) < sqrt(time^2/4 - distance)
+      t in (time/2-sqrt, time/2+sqrt)
+     */
+    println!("{answer}")
+}
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,12 @@ mod day2;
 mod day3;
 mod day4;
 mod day5;
+mod day6;
 
 fn main() {
-  day5::day5()
+  day6::day6()
+}
+
+pub fn string_to_numbers(s: &str) -> Vec<u64> {
+  s.split(" ").filter(|w| !w.is_empty()).map(|w| w.parse().unwrap()).collect()
 }
