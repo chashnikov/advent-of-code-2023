877539d6b829e7d06896d822834055ed40cb99a9
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2023-12-25 22:14:17 +0100

    [day17] solve part 2

--- src/day17.rs
+++ src/day17.rs
@@ -5,22 +5,24 @@ use itertools::Itertools;
 use priority_queue::DoublePriorityQueue;
 use crate::{Direction, DIRECTIONS, EAST, PositionI32, read_to_string, SOUTH};
 
+const VARIANTS: usize = 4 * 10;
+
 pub fn solve() {
     let content = read_to_string("17-full.txt");
     let grid = Array2D::from_rows(content.lines().map(|line| line.chars().map(|c| c.to_digit(10).unwrap()).collect_vec()).collect_vec().as_slice()).expect("correct grid");
     let x_max = grid.num_columns();
     let y_max = grid.num_rows();
-    let mut min_cost: Array2D<[u32; 12]> = Array2D::filled_with([1_000_000_000; 12], grid.num_rows(), grid.num_columns());
+    let mut min_cost: Array2D<[u32; VARIANTS]> = Array2D::filled_with([1_000_000_000; VARIANTS], grid.num_rows(), grid.num_columns());
     let mut next : DoublePriorityQueue<(PositionI32, Impulse), u32> = DoublePriorityQueue::new();
 
-    let impulses = (0..=2).flat_map(|r| DIRECTIONS.map(|d| Impulse {
+    let impulses = (0..=9).flat_map(|r| DIRECTIONS.map(|d| Impulse {
         direction: d,
         remaining: r,
     })).collect_vec();
-    assert_eq!(impulses.len(), 12);
+    assert_eq!(impulses.len(), VARIANTS);
 
-    next.push((PositionI32 { x: 0, y: 1}, Impulse {direction: SOUTH, remaining: 2}), grid[(1,0)]);
-    next.push((PositionI32 { x: 1, y: 0}, Impulse {direction: EAST, remaining: 2}), grid[(0,1)]);
+    next.push((PositionI32 { x: 0, y: 1}, Impulse {direction: SOUTH, remaining: 9}), grid[(1,0)]);
+    next.push((PositionI32 { x: 1, y: 0}, Impulse {direction: EAST, remaining: 9}), grid[(0,1)]);
     while !next.is_empty() {
         let ((position, impulse), cost) = next.pop_min().unwrap();
         // println!("Reached {} with {}", position, cost);
@@ -31,10 +33,11 @@ pub fn solve() {
         }
         min_cost[index][impulse_index] = cost;
         for direction in DIRECTIONS {
-            if direction == impulse.direction.opposite() || direction == impulse.direction && impulse.remaining == 0 {
+            if direction == impulse.direction.opposite() || direction == impulse.direction && impulse.remaining == 0
+               || direction != impulse.direction && impulse.remaining > 6 {
                 continue
             }
-            let remaining = if direction == impulse.direction { impulse.remaining - 1 } else { 2 };
+            let remaining = if direction == impulse.direction { impulse.remaining - 1 } else { 9 };
             let new_position = position + direction;
             if new_position.x < 0 || new_position.x >= x_max as i32 || new_position.y < 0 || new_position.y >= y_max as i32 {
                 continue
@@ -51,7 +54,11 @@ pub fn solve() {
             }
         }
     }
-    let answer = min_cost[(y_max-1, x_max-1)].iter().min().unwrap();
+    let answer = min_cost[(y_max-1, x_max-1)].iter().enumerate()
+        .filter(|(i, _)| impulses.get(*i).unwrap().remaining <= 6)
+        .map(|(_, v)| v)
+        .min()
+        .unwrap();
     println!("{answer}")
 }
 
