67d6f391f1942d14953d5a663432dc58fc6f0548
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2023-12-28 09:38:15 +0100

    [day18] solve part 2

--- src/day18.rs
+++ src/day18.rs
@@ -2,16 +2,17 @@ use regex::Regex;
 use crate::{Direction, EAST, NORTH, PositionI32, read_to_string, SOUTH, WEST};
 
 pub fn solve() {
-    let content = read_to_string("18.txt");
-    let re = Regex::new(r"([A-Z]) +(\d+) +\(#[0-9a-f]+\)").unwrap();
+    let content = read_to_string("18-full.txt");
+    let re = Regex::new(r"([A-Z]) +(\d+) +\(#([0-9a-f]+)\)").unwrap();
     let mut moves : Vec<(Direction, u32)> = Vec::new();
     let mut positions : Vec<PositionI32> = Vec::new();
     let mut current = PositionI32 { x:0, y:0 };
     positions.push(current.clone());
     content.lines().for_each(|line| {
         let cap = re.captures(line).unwrap();
-        let direction = parse_direction(cap[1].chars().next().unwrap());
-        let length = cap[2].parse::<u32>().unwrap();
+        let coded = cap[3].chars().as_str();
+        let direction = parse_direction(coded.chars().nth(5).unwrap());
+        let length = u32::from_str_radix(&coded[0..5], 16).expect("must be integer");
         moves.push((direction, length));
         current = current + direction * length as i32;
         positions.push(current);
@@ -25,14 +26,15 @@ pub fn solve() {
     current = PositionI32 { x:0, y:0 };
     for (i, (direction, length)) in moves.iter().enumerate() {
         let delta = match *direction {
-            NORTH =>  (x_max + 1 - current.x) - (current.x - x_min),
-            EAST => (y_max + 1 - current.y) - (current.y - y_min),
-            SOUTH => (current.x + 1 - x_min) - (x_max - current.x),
-            WEST => (current.y + 1 - y_min) - (y_max - current.y),
+            NORTH =>  (x_max - current.x) - (current.x - x_min),
+            EAST => (y_max - current.y) - (current.y - y_min),
+            SOUTH => (current.x - x_min) - (x_max - current.x),
+            WEST => (current.y - y_min) - (y_max - current.y),
             _ => panic!("unknown direction {}", direction)
         };
         total += delta as i64 * (*length as i64);
         current = current + *direction * *length as i32;
+
         let next_direction = moves[(i+1) % moves.len()].0;
         if next_direction == *direction {
             total += 2;
@@ -46,6 +48,7 @@ pub fn solve() {
         else {
             total += 1;
         }
+        total += (*length as i64 - 1)*2;
     }
     let answer = total / 4;
     println!("{answer}");
@@ -53,10 +56,10 @@ pub fn solve() {
 
 fn parse_direction(c: char) -> Direction {
     match c {
-        'U' => NORTH,
-        'D' => SOUTH,
-        'R' => EAST,
-        'L' => WEST,
+        '3' => NORTH,
+        '1' => SOUTH,
+        '0' => EAST,
+        '2' => WEST,
         _ => panic!("unknown direction {}", c)
     }
 }
