fb9b0eabdcda424111c4079f7a3500f30935b78f
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2024-01-02 22:31:17 +0100

    [day23] initial

---
+++ src/day23.rs
@@ -0,0 +1,7 @@
+use crate::read_to_string;
+
+pub fn solve() {
+    let content = read_to_string("23.txt");
+    let answer = 0;
+    println!("{answer}");
+}
--- src/main.rs
+++ src/main.rs
@@ -26,6 +26,7 @@ mod day19;
 mod day20;
 mod day21;
 mod day22;
+mod day23;
 
 fn main() {
   let option = env::args().nth(1);
